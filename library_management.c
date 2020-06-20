4c79891a5c73b57cac6f01fe357bbeb36a970403
thefalse9dev <63153003+thefalse9dev@users.noreply.github.com>
2020-06-20 13:45:17 +0530

    search method

--- library_management.c
+++ library_management.c
@@ -12,6 +12,8 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+void search(int);
+void reserve_book(int);
 struct user{
     /*A user has an id, username, passowrd, cart of 5 books maximum
     , notifcations of 5 notifs max*/
@@ -39,26 +41,46 @@ struct user members[1000];
 int librarysize=0;
 int membersize=0;
 int b_id=1;
-void search(){
+void search(int id){
     /*Search for a particular title*/
+    int userid =id;
     char title[50];
+    int flag=0;
     printf("Enter the book title to be searched : \n");
     scanf("%[^\n]%*c", title);
+    for(int i=0;i<librarysize;i++){
+        if(strcmp(title,library[i].bookname) == 0){
+            flag++;
+            printf("Book ID : &d \n",library[i].bookid);
+            printf("Title : &c \n",library[i].bookname);
+            printf("Author : &c \n",library[i].bookauthor);
+            printf("issue number : &d \n",library[i].issue);
+        }
+    }
+    int ch;
+    if(flag==0){
+        printf("Sorry the book is unavailable, would you like to place an order for the same \n");
+        printf("Enter 1 to place an order \n");
+        scanf("%d",&ch);
+        if(ch==1)
+            reserve_book(userid);
+    }
     /*Search for a particular book_id*/
     /*Search for by title and author*/
     
 }
-void checkout(int id){
+ void checkout(int id){
 
     /* After searching the user will be asked to enter the id of the book he wants
     to checkout, if the user doesn't have a book of the same title in his cart and
     library has the book, its held by will be set to user_id and due_date will be 
     generated, will be added to his cart at cart[cartsize] and cartsize++, librarysize -- */
 }
-void reserve(int id,char title[],char author[]){
+  void reserve_book(int id){
     /*A new field will be created in the library with 
     requested_by = user_id,bookname =title[],bookauthor=author[]
     bookid = b_id, b_id++*/
+    
 }
 void delete_reservation(int id){
     /* shift all the books after the book id position to delete the reservation if held_by =-1*/
@@ -106,8 +128,7 @@ void welcome(){
     
 }
 int main(){
-    
-     search();
+    search(2);
      welcome();
     return 0;
 }
